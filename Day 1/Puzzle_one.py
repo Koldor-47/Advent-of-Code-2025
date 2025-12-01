09a1872b5c49c58a9f6fd3008ef27785b0eb4426
nick <nickhowlett13@gmail.com>
2025-12-02 05:19:01 +0800

    completed part b

--- Day 1/Puzzle_one.py
+++ Day 1/Puzzle_one.py
@@ -24,32 +24,39 @@ def get_moveby(current_number, moveby, direction):
     else:
         return current_number
     
-def get_moveby_partb(current_number, moveby, direction):
-
+def get_moveby_partb(starting_pointer, amount_moved, direction):
     past_zero = 0
+    real_position = 0
+    move_difference = amount_moved
+
+    
+    if amount_moved >= 100:
+        past_zero = amount_moved // 100
+        move_difference = amount_moved % 100
 
     if direction.lower() == "l":
-        current_number = current_number - moveby
+        current_position = starting_pointer - move_difference
     elif direction.lower() == 'r':
-        current_number = current_number + moveby
+        current_position = starting_pointer + move_difference
+
+
+    if current_position == 0:
+        past_zero += 1
+    elif current_position <= 99 and current_position > 0:
+        real_position = current_position
+    elif current_position > 99 or current_position < 0:
+        if starting_pointer != 0:
+            past_zero += 1
+        real_position = current_position % 100
+    
+    
+     
+
+
     
 
-    if current_number > 99 and current_number <= 100:
-        current_number = (current_number % 100)
-        past_zero += 1
-    elif current_number > 100:
-        past_zero = current_number // 100
-        current_number = (current_number % 100)
 
-    elif current_number <= 0 and current_number >= -100:
-        current_number = (current_number % 100)
-        past_zero += 1
-    elif current_number < -99:
-        past_zero = abs(current_number // 100)
-        current_number = (current_number % 100)
-        
-
-    return (current_number, past_zero)
+    return (real_position, past_zero)
 
 def main():
     rotation_instructions = read_file(r"Day 1\puzzle_input.txt")
@@ -75,33 +82,15 @@ def main():
 if __name__ == "__main__":
 
     
-    t1 = get_moveby_partb(10, 15, "L")
-    t2 = get_moveby_partb(10, 200, "L")
-    t3 = get_moveby_partb(50, 50, "R")
-    t4 = get_moveby_partb(50, 150, "R")
-    t5 = get_moveby_partb(50, 1000, "R")
-    t6 = get_moveby_partb(50, 1000, 'L')
-    t7 = get_moveby_partb(0, 1, "L")
-    t8 = get_moveby_partb(0, 300, "R")
-    t10 = get_moveby_partb(45, 958, "R")
-    t11 = get_moveby_partb(98, 103, "L")
-    t12 = get_moveby_partb(95, 5, "R")
-    t13 = get_moveby_partb(5, 105, "L")
-    t14 = get_moveby_partb(5, 60, "L")
+    t1 = get_moveby_partb(98, 1, "R")
+    t2 = get_moveby_partb(1, 2, "L")
+    t3 = get_moveby_partb(12, 300, "R")
+    t4 = get_moveby_partb(12, 312, "L")
 
     print(t1)
     print(t2)
     print(t3)
     print(t4)
-    print(t5)
-    print(t6)
-    print(t7)
-    print(t8)
-    print(t10)
-    print(t11)
-    print(t12)
-    print(t13)
-    print(t14)
+
 
     main()
-    
