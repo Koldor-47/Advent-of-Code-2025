e1e5b841313b61ab751fb643c6d5a6aff1146cc0
--==| Nick |==-- <nickhowlett13@gmail.com>
2025-12-06 23:35:10 +0800

    completed day 2

--- Day2/Puzzle_two.py
+++ Day2/Puzzle_two.py
@@ -48,9 +48,58 @@ def puzzle_b(first_id, last_id):
     first_num = int(first_id)
     last_num = int(last_id)
 
-    print(f"{first_num} length is {len(first_id)}")
+    if len(last_id) > len(first_id):
+        invalid_ids = number_change(first_id, last_id)
+    else:
+        # i think i need recusion hmmmm
+        invalid_ids = get_invalid_ids(first_id, last_id, len(first_id))
 
-    return [0]
+    return invalid_ids
+
+def get_invalid_ids(first_id, last_id, id_length):
+    invalid_ids = []
+    patterns = []
+    keep_going = True
+    test_length = id_length
+    while keep_going:    
+        if id_length // test_length == id_length / test_length:
+            pattern_len = int(id_length / test_length)
+            pattern = first_id[:pattern_len]
+            below_lastid = True
+            while below_lastid:
+                num = int(pattern * test_length)
+                if num <= int(last_id):
+                    if num <= int(last_id) and num >= int(first_id) and len(str(num)) > 1:
+                        invalid_ids.append(num)
+                else:
+                    below_lastid = False
+                
+                up = int(pattern) + 1
+                pattern = str(up)
+            
+
+        test_length -= 1
+        if test_length <= 1: 
+            #print("breaking")
+            break
+    
+    print(invalid_ids)
+    return invalid_ids
+
+
+def number_change(first_id, last_id):
+    invalid_ids = []
+    upper_first = len(first_id) * "9"
+    num_upper = int(upper_first)
+    lower_last = str(num_upper + 1)
+
+    lower = get_invalid_ids(first_id, upper_first, len(first_id))
+    upper = get_invalid_ids(lower_last, last_id, len(lower_last))
+
+    invalid_ids.extend(lower)
+    invalid_ids.extend(upper)
+
+    return invalid_ids
 
 
 def go_though_ids(puzzle_data):
@@ -58,19 +107,25 @@ def go_though_ids(puzzle_data):
     for id in puzzle_data:
         first, last = id.split("-")
         bad_ids = puzzle_b(first, last)
+        invalid_ids.extend(bad_ids)
+    
+    return list(set(invalid_ids))
     
         
 
 if __name__ == "__main__":
     print("Day 2 Advent Of Code 2025")
 
-    test_input = r"Day2\test_puzzle_input.txt"
-
+    test_input = r"Day2/puzzle_input.txt"
     puzzle_data = read_puzzle_data(test_input)
-
     answer = go_though_ids(puzzle_data)
 
-    
+    print(sum(answer))
+
+    # part b too low
+    # 21966241186
+    # 27469417393
+
 
     # too Low 11277648113,
     #         16793817782 
