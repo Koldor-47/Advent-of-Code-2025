47dcc94c0d441fd5bf8611be8b9e17b940194901
--==| Nick |==-- <nickhowlett13@gmail.com>
2025-12-09 00:24:22 +0800

    first real try....

--- day5/day_5.py
+++ day5/day_5.py
@@ -27,15 +27,43 @@ def read_ingredient_db(file_path):
     return data
 
 def make_fresh(fresh_ranges):
+    new_fresh_ranges = []
     print("Making Ranges....")
-    fresh_ingredients = {}
+    low = None
+    high = 0
+    sorted_ranges = sorted(fresh_ranges)
+    for index, r in enumerate(sorted_ranges):
+        if index + 1 < len(sorted_ranges):
+            next_item = index + 1
+            current_high = r[1]
+            current_low = r[0]
+            next_high = sorted_ranges[next_item][1]
+            next_low= sorted_ranges[next_item][0]
+            if next_low <= current_high and next_low >= current_low:
+                #print(f"{r} - {sorted_ranges[next_item]}")
+                if low is None:
+                    low = current_low
+                elif low > current_low:
+                    low = current_low
+                if high < next_high:
+                    high = next_high
+            elif low is not None and high is not None:
+                new_fresh_ranges.append(range(low, high+1))
+                low = None
+                high = 0
+            else:
+                new_fresh_ranges.append(range(r[0], r[1]+1))
 
-    for the_range in fresh_ranges:
-        for item in range(the_range[0], the_range[1] + 1):
-            if item not in fresh_ingredients:
-                fresh_ingredients[item] = "Fresh"
+        if index + 1 == len(sorted_ranges):
+            if low is not None:
+                new_fresh_ranges.append(range(low, high+1))
+            elif low is None:
+                new_fresh_ranges.append(range(r[0], r[1]+1))
+
+
     
-    return fresh_ingredients
+    return new_fresh_ranges
+
 
 
 if __name__ == "__main__":
@@ -46,3 +74,22 @@ if __name__ == "__main__":
     data = read_ingredient_db(TEST)
 
     the_fresh_ingredients = make_fresh(data["fresh_ranges"])
+    ingredients = sorted(data["ingredients"], reverse=True)
+    fresh = 0
+    count = 0
+    print(f"the ingredients {len(ingredients)} and the fresh ids {len(the_fresh_ingredients)}")
+    while count < len(the_fresh_ingredients):
+        for ingredient in range(len(ingredients)-1, 0, -1):
+            if ingredients[ingredient] in the_fresh_ingredients[count]:
+                fresh += 1
+                ingredients.pop()
+
+        count += 1
+    
+    print(fresh)
+
+
+# too Low 752 hmmmm
+
+
+
