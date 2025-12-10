290a73096da23577ce825138ecd1e782e12e8f68
--==| Nick |==-- <nickhowlett13@gmail.com>
2025-12-10 19:21:14 +0800

    day 6 part a completed

--- Day6/day6.py
+++ Day6/day6.py
@@ -1,8 +1,9 @@
 # Advent of Code Day 6
+import math
 
 def read_homework(file_path):
     numbers = []
-    math_lines = 3
+    math_lines = 4
     instructions_line = -1 # last line
     instructions = []
 
@@ -12,16 +13,45 @@ def read_homework(file_path):
             line = line.strip()
             data.append(line)
         numbers = data[:math_lines]
-        instructions = data[instructions_line]
-    
+        instructions = data[-1]
+        instructions = instructions.strip().split()
+
     maths_workbook = {
         "numbers" : numbers,
         "instructions" : instructions
-
     }
 
     return maths_workbook
  
+def str_to_int(math_numbers):
+    all_rows = []
+    for row in math_numbers:
+        numbers = row.split()
+        numbers = list(map(int, numbers))
+        all_rows.append(numbers)
+
+    return all_rows
+
+    #for str_list in txt_numbers:
+    #    numbers = list(map(int, str_list))
+    #    real_number.append(numbers)
+
+   # return numbers
+
+def check_maths(maths_book, number_questions):
+    total = []
+    for problem_num in range(0, number_questions):
+        problem_numbers = [maths_book["real_numbers"][0][problem_num], maths_book["real_numbers"][1][problem_num], maths_book["real_numbers"][2][problem_num], maths_book["real_numbers"][3][problem_num]]
+        if maths_book["instructions"][problem_num] == "*":
+            answer = math.prod(problem_numbers)
+        elif maths_book["instructions"][problem_num] == "+":
+            answer = sum(problem_numbers)
+        
+        
+        print(f"{maths_book["real_numbers"][0][problem_num]} {maths_book["instructions"][problem_num]} {maths_book["real_numbers"][1][problem_num]} {maths_book["instructions"][problem_num]} {maths_book["real_numbers"][2][problem_num]} = {answer}")
+        total.append(answer)
+    
+    return sum(total)
 
 if __name__ == "__main__":
     print("day 6 Advent of Code")
@@ -31,4 +61,12 @@ if __name__ == "__main__":
 
     maths_book = read_homework(REAL)
 
-    print(maths_book["numbers"][0].split())
+    maths_book["real_numbers"] = str_to_int(maths_book["numbers"])
+
+    part_a = check_maths(maths_book, 1000)
+
+    print(part_a)
+
+
+# too Low 19745445035
+
